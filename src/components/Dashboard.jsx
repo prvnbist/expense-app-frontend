80af0485414d6f056c6f17a56113bcbd96e8058e
unknown <prvnbist@gmail.com>
2018-11-06 00:06:25 +0530

    Fixed Typo

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,15 +1,15 @@
 import React, {Component} from 'react';
 
 import Header from './Header';
-import Main from './main';
+import Main from './Main';
 
 export default class Dashboard extends Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <Header />
-        <Main />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="wrapper">
+                <Header/>
+                <Main/>
+            </div>
+        )
+    }
 }
