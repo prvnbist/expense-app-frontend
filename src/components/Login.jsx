9a0962eae4fa442f31c00b2c5af45a4fedb33a8d
unknown <prvnbist@gmail.com>
2018-11-06 00:07:01 +0530

    Added Ant Design Tags

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import {Mutation} from 'react-apollo';
 import gql from 'graphql-tag';
+import { Link } from 'react-router-dom';
+
+// Ant Design
+import { Input, Row, Col, Button } from 'antd';
 
 const LOGIN_MUTATION = gql `
   mutation LoginMutation($username: String!, $password: String!) {
@@ -12,39 +16,42 @@ export default class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            username: 'shellshoreman',
-            password: 'shell'
+            username: '',
+            password: ''
         }
     }
     submitForm = ({login}) => {
-        // document.cookie = `access_token=Bearer ${login}`;
         localStorage.clear();
         localStorage.setItem('access_token',login);
+		this.props.history.push('/dashboard');
     }
     render() {
         const {username, password} = this.state;
         return (
-            <div>
-                <label>Username</label>
-                <input
-                    value={username}
-                    onChange={e => this.setState({username: e.target.value})}
-                    type="text"
-                    placeholder="Enter your username"/>
-                <label>Password</label>
-                <input
-                    value={password}
-                    onChange={e => this.setState({password: e.target.value})}
-                    type="password"
-                    placeholder="Enter your password"/>
-                <Mutation
-                    mutation={LOGIN_MUTATION}
-                    variables={{username,password}}
-                    onCompleted={data => this.submitForm(data)}>
-                    {mutation => (
-                        <button onClick={mutation}>Login</button>
-                    )}
-                </Mutation>
+            <div className="wrapper center-content">
+                <Row gutter={16} className="loginForm">
+                    <Col span={24}>
+                    <label className="inputLabel">Username</label>
+                    <Input
+                        value={username}
+                        onChange={e => this.setState({username: e.target.value})}
+                        type="text"
+                        placeholder="Enter your username"/>
+                    <label className="inputLabel">Password</label>
+                    <Input
+                        value={password}
+                        onChange={e => this.setState({password: e.target.value})}
+                        type="password"
+                        placeholder="Enter your password"/>
+                    <Mutation
+                        mutation={LOGIN_MUTATION}
+                        variables={{username,password}}
+                        onCompleted={data => this.submitForm(data)}>
+                        {mutation => <Button type="primary" size="large" onClick={mutation}>Login</Button>}
+                    </Mutation>
+                    <span className="loginOption"> or <Link to="/signup">Sign Up</Link></span>
+                    </ Col>
+                </Row>
             </div>
         )
     }
