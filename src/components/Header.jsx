414d5defdba77f30956f72474bf0df3910552a2e
Praveen Bisht <prvnbist@gmail.com>
2018-11-08 21:42:27 +0530

    Added current user information.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,49 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+import {Query} from 'react-apollo';
+import gql from 'graphql-tag';
+
+const CURRENT_USER = gql `
+  query currentUser {
+    me {
+        id
+        name
+        username
+        email
+        gender
+        expenses {
+          spentOn
+        }
+      }
+  }
+`;
+
+const LoggedInUser = () => (
+    <Query query={CURRENT_USER}>
+        {({loading, error, data}) => {
+            if (loading) 
+                return "Loading...";
+            if (error) 
+                return `Error! ${error.message}`;
+            const {name} = data.me;
+            return name;
+        }}
+    </Query>
+);
 
 export default class Header extends Component {
+    logOut = () => {
+        localStorage.removeItem('access_token');
+    }
     render() {
         return (
             <header>
                 <nav>
                     <span id="logo">Expense Manager</span>
                     <div id="profile-options">
-                        <span id="user-name">Praveen</span>
+                        <span id="user-name"><LoggedInUser /></span>
+                        <Link to="/"><button id="logout" onClick={this.logOut}>Logout</button></Link>
                     </div>
                 </nav>
                 <div id="total-expenses">
