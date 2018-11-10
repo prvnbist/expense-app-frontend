e9e8117372b3aa550b9e909ecabc26230ec22c43
Praveen Bisht <prvnbist@gmail.com>
2018-11-10 16:37:20 +0530

    Added content on homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 import NavBar from './NavBar';
 
 // Ant Design
 import {
-    Layout
+    Layout,
+    Row,
+    Col,
+    Button
 } from 'antd';
 
 export default class Home extends Component {
@@ -12,6 +16,17 @@ export default class Home extends Component {
         return (
             <Layout className="layout">
                 <NavBar />
+                <main id="homepage-content">
+                <Row gutter={16} >
+                    <Col span={12}>
+                        <h1 id="heading">Easily manage your expenses.</h1>
+                        <Button type="primary" size="large"><Link to='/login'>LOGIN</Link></Button>
+                    </Col>
+                    <Col span={12}>
+                        <img src={process.env.PUBLIC_URL + '/images/illo.svg'} alt="" id="homepage-illo"/>
+                    </Col>
+                    </Row>
+                </main>
             </Layout>
         )
     }
