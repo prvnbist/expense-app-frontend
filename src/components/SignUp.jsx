d423773bbad7ef61bb04002a7a22b77c1c8351a4
Praveen Bisht <prvnbist@gmail.com>
2019-01-02 15:47:28 +0530

    updated error messages and refactored code

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -94,7 +94,7 @@ export default class SignUp extends Component {
                                         "exists": "Email already exists!",
                                     }], 
                                     "username": [{
-                                        "valid": "Username must have only alphabets or numbers!",
+                                        "valid": "Username must have _alpha_numerics",
                                         "exists": "Username already exists!",
                                         "length": "Username must be 4 letters long!"
                                     }], 
@@ -111,15 +111,15 @@ export default class SignUp extends Component {
 
                                 if(name && name.length < 4) {
                                     document.getElementById("name-error").style.display = 'block';
-                                    document.getElementById('name-error').innerHTML = errorsList.name[0].length;
+                                    document.getElementById('name-error').innerHTML = errorsList.name[0]["length"];
                                 }
                                 
                                 const userNameRegex = /^[a-zA-Z0-9-_]+$/;
                                 let validUserName = userNameInput => userNameRegex.test(userNameInput);
                                 
-                                if(username && username.length < 4 && !validUserName(username)) {
+                                if(username && username.length < 4) {
                                     document.getElementById("username-error").style.display = 'block';
-                                    document.getElementById('username-error').innerHTML = errorsList.username[0].length;
+                                    document.getElementById('username-error').innerHTML = errorsList.username[0]["length"];
                                 }
                                 
                                 if(username && !validUserName(username)) {
@@ -147,7 +147,7 @@ export default class SignUp extends Component {
 
                                 if(password.length < 4 && password !== '') {
                                     document.getElementById("password-error").style.display = 'block';
-                                    document.getElementById('password-error').innerHTML = errorsList.password[0].length;
+                                    document.getElementById('password-error').innerHTML = errorsList.password[0]["length"];
                                 }
 
                                 const passwordRegex = /^(?=S*[a-z])(?=S*[A-Z])(?=\S*\d)(?=S*[^\W\s])\S{8,30}$/i;
