df839fcf2370c216097f9d132610e65f920ec602
Praveen Bisht <prvnbist@gmail.com>
2019-07-05 17:35:33 +0530

    Changes: Minor

--- src/components/ExpenseCard.jsx
+++ src/components/ExpenseCard.jsx
@@ -2,26 +2,25 @@ import React from 'react';
 import {Mutation} from 'react-apollo';
 import DELETE_EXPENSE_MUTATION from '../queries/DeleteExpense';
 
-export default class Expense extends React.Component {
-    reLoadPageWithNewData = _ => {
-        window
-            .location
-            .reload();
-    }
-    render() {
-        return <div
+const Expense = props => {
+    return (
+        <div
             className="expense-card"
-            key={this.props.item.id}
+            key={props.item.id}
             style={{
-            borderTop: this.props.item.type === "plus"
+            borderTop: props.item.type === "plus"
                 ? '3px solid #318FFE'
                 : '3px solid #EC1A1A'
         }}>
             <div className='top-row'>
-                <span title={this.props.item.spentOn} className="spentOn">
-                    {this.props.item.spentOn.length > 30
-                        ? this.props.item.spentOn.slice(0, 20) + '...'
-                        : this.props.item.spentOn}
+                <span title={props.item.spentOn} className="spentOn">
+                    {props.item.spentOn.length > 30
+                        ? this
+                            .props
+                            .item
+                            .spentOn
+                            .slice(0, 20) + '...'
+                        : props.item.spentOn}
                 </span>
                 <div
                     style={{
@@ -31,21 +30,21 @@ export default class Expense extends React.Component {
                     <span
                         className="amount"
                         style={{
-                        color: this.props.item.type === "plus"
+                        color: props.item.type === "plus"
                             ? '#318FFE'
                             : '#EC1A1A'
-                    }}>{this.props.item.type === "plus"
+                    }}>{props.item.type === "plus"
                             ? '+'
-                            : '-'} {parseInt(this.props.item.amount).toLocaleString("en-IN", {
+                            : '-'} {parseInt(props.item.amount).toLocaleString("en-IN", {
                             style: 'currency',
                             currency: "INR"
                         })}</span>
                     <Mutation
                         mutation={DELETE_EXPENSE_MUTATION}
                         variables={{
-                        id: this.props.item.id
+                        id: props.item.id
                     }}
-                        onCompleted={data => this.reLoadPageWithNewData(data)}>
+                        refetchQueries={() => ['usersExpenses']}>
                         {mutation => <button className="expense__action" onClick={mutation}>
                             <i className="material-icons">delete</i>
                         </button>}
@@ -55,19 +54,19 @@ export default class Expense extends React.Component {
             <div className='mid-row'>
                 <span className="category">
                     <i className='material-icons'>category</i>
-                    <span>{this.props.item.category}</span>
+                    <span>{props.item.category}</span>
                 </span>
                 <span className="time">
                     <i className='material-icons'>access_time</i>
                     <span>
-                        {new Date(Number(this.props.item.createdAt))
+                        {new Date(Number(props.item.createdAt))
                             .toTimeString()
                             .slice(0, 5)}
                     </span>
                 </span>
                 <span className="date">
                     <i className='material-icons'>event</i>
-                    <span>{new Date(Number(this.props.item.createdAt))
+                    <span>{new Date(Number(props.item.createdAt))
                             .toDateString()
                             .replace(' 2018', '')}</span>
                 </span>
@@ -75,11 +74,13 @@ export default class Expense extends React.Component {
             <div className='bottom-row'>
                 <span className="description">
                     <i className='material-icons'>description</i>
-                    <span>{this.props.item.description
-                        ? this.props.item.description
-                        : 'No description.'}</span>
-                        </span>
+                    <span>{props.item.description
+                            ? props.item.description
+                            : 'No description.'}</span>
+                </span>
             </div>
         </div>
-    }
+    );
 }
+
+export default Expense;
