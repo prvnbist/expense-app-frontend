08c8b0efa931053e468386a0ff1eba8d8a24c35a
Praveen Bisht <prvnbist@gmail.com>
2019-08-23 21:15:10 +0530

    Converted Mutation to useMutation

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,242 +1,222 @@
-import React, { Component } from "react";
-import { Mutation } from "react-apollo";
-import { Formik } from "formik";
-import * as Yup from "yup";
+import React from 'react'
+import { useMutation } from '@apollo/react-hooks'
+import { Formik } from 'formik'
+import * as Yup from 'yup'
 
-import SIGNUP_MUTATION from "../queries/SignUpMutation";
-export default class SignUp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: false,
-      buttonText: "Sign Up",
-      authError: null
-    };
-  }
-  showPassword = e => {
-    if (!this.state.show) {
-      e.target.innerText = "visibility";
-      document.getElementById("password-input").type = "text";
-      this.setState({
-        show: !this.state.show
-      });
-      return;
-    }
-    e.target.innerText = "visibility_off";
-    document.getElementById("password-input").type = "password";
-    this.setState({
-      show: !this.state.show
-    });
-  };
-  submitForm = ({ signup }) => {
-    localStorage.clear();
-    localStorage.setItem("access_token", signup);
-    this.props.history.push("/dashboard");
-  };
-  render() {
-    const SignupSchema = Yup.object().shape({
-      name: Yup.string()
-        .min(4, "Name is too short!")
-        .max(50, "Name is too long!")
-        .required("Name is required!"),
-      email: Yup.string()
-        .email("Please enter a valid email!")
-        .required("Email is required!"),
-      username: Yup.string()
-        .min(4, "Username is too short!")
-        .max(50, "Username is too long!")
-        .matches(/^[a-zA-Z0-9-_]+$/, "Username must have _alpha_numerics")
-        .required("Username is required!"),
-      password: Yup.string()
-        .min(8, "Password is too short!")
-        .max(20, "Password is too long!")
-        .matches(
-          /(?=^.{8,20}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s)[0-9a-zA-Z!@#$%^&*()]*$/,
-          "Password must have atleast one lowercase letter, one uppercase letter, one digit" +
-            " and one special character."
-        )
-        .required("Password is required!")
-    });
-    return (
-      <div className="signup">
-        <h2 className="page-heading">
-          Register to start managing your expenses.
-        </h2>
-        <Mutation
-          mutation={SIGNUP_MUTATION}
-          onError={err => {
-            const errors = err.message
-              .split(",")
-              .map(i => i.split(":"))
-              .map(i => i.reverse()[0])
-              .map(i => i.trim());
-            this.setState({
-              authError: errors,
-              buttonText: "Sign Up"
-            });
-          }}
-          onCompleted={data => this.submitForm(data)}
-        >
-          {signUpMutation => (
-            <Formik
-              initialValues={{
-                username: "prvnbist",
-                password: "xLr8e47@",
-                name: "Bruno Mars",
-                email: "prvnbist@gmail.com",
-                gender: "M"
-              }}
-              validationSchema={() =>
-                this.setState({
-                  authError: null
-                }) || SignupSchema
-              }
-              onSubmit={(values, { setSubmitting }) => {
-                this.setState({ buttonText: "Signing Up..." });
-                setTimeout(() => {
-                  signUpMutation({
-                    variables: {
-                      username: values.username,
-                      password: values.password,
-                      name: values.name,
-                      email: values.email,
-                      gender: values.gender
-                    }
-                  });
-                  setSubmitting(false);
-                }, 400);
-              }}
-            >
-              {({
-                values,
-                errors,
-                touched,
-                handleChange,
-                handleBlur,
-                handleSubmit,
-                isSubmitting
-              }) => (
-                <form onSubmit={handleSubmit}>
-                  {this.state.authError &&
-                    this.state.authError.map(err => (
-                      <span
-                        key={err}
-                        id="username-error"
-                        className="error-message"
-                      >
-                        {err}
-                      </span>
-                    ))}
-                  <div className="field">
-                    <input
-                      type="text"
-                      placeholder="Enter your name"
-                      id="name-input"
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      name="name"
-                      value={values.name}
-                    />
-                    <label htmlFor="name-input">
-                      <i className="material-icons">account_circle</i>
-                    </label>
-                  </div>
-                  {touched.name && errors.name && (
-                    <span id="name-error" className="error-message">
-                      {errors.name}
-                    </span>
-                  )}
-                  <div className="field">
-                    <input
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.email}
-                      type="text"
-                      name="email"
-                      placeholder="Enter your email"
-                      id="email-input"
-                    />
-                    <label htmlFor="email-input">
-                      <i className="material-icons">email</i>
-                    </label>
-                  </div>
-                  {touched.email && errors.email && (
-                    <span id="email-error" className="error-message">
-                      {errors.email}
-                    </span>
-                  )}
-                  <div className="field">
-                    <input
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.username}
-                      type="text"
-                      name="username"
-                      placeholder="Enter your username"
-                      id="username-input"
-                    />
-                    <label htmlFor="username-input">
-                      <i className="material-icons">alternate_email</i>
-                    </label>
-                  </div>
-                  {touched.username && errors.username && (
-                    <span id="username-error" className="error-message">
-                      {errors.username}
-                    </span>
-                  )}
-                  <div className="field">
-                    <input
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.password}
-                      type="password"
-                      name="password"
-                      placeholder="Enter your password"
-                      id="password-input"
-                    />
-                    <label htmlFor="password-input">
-                      <i
-                        className="material-icons"
-                        style={{
-                          cursor: "pointer"
-                        }}
-                        onClick={e => this.showPassword(e)}
-                      >
-                        visibility_off
-                      </i>
-                    </label>
-                  </div>
-                  {touched.password && errors.password && (
-                    <span id="password-error" className="error-message">
-                      {errors.password}
-                    </span>
-                  )}
-                  <div className="field select-field">
-                    <select
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      className="select-dropdown"
-                      value={values.gender}
-                      name="gender"
-                    >
-                      <option value="M">Male</option>
-                      <option value="F">Female</option>
-                    </select>
-                  </div>
-                  <button
-                    type="submit"
-                    className="btn btn__primary"
-                    onClick={e => (e.target.innerText = "Signing Up...")}
-                    disabled={isSubmitting}
-                  >
-                    {this.state.buttonText}
-                  </button>
-                </form>
-              )}
-            </Formik>
-          )}
-        </Mutation>
-      </div>
-    );
-  }
+import SIGNUP_MUTATION from '../queries/SignUpMutation'
+
+const SignUp = props => {
+	const [show, toggleShow] = React.useState(false)
+	const [authError, setAuthError] = React.useState(null)
+	const [signupUser, { loading: mutationLoading }] = useMutation(
+		SIGNUP_MUTATION,
+		{
+			onError: err => {
+				const errors = err.message
+					.split(',')
+					.map(i => i.split(':'))
+					.map(i => i.reverse()[0])
+					.map(i => i.trim())
+				setAuthError(errors)
+			},
+			onCompleted: ({ signup }) => {
+				localStorage.clear()
+				localStorage.setItem('access_token', signup)
+				props.history.push('/dashboard')
+			},
+		}
+	)
+
+	const showPassword = e => {
+		e.target.innerText = show ? 'visibility_off' : 'visibility'
+		document.getElementById('password-input').type = show
+			? 'password'
+			: 'text'
+		toggleShow(!show)
+	}
+
+	const SignupSchema = Yup.object().shape({
+		name: Yup.string()
+			.min(4, 'Name is too short!')
+			.max(50, 'Name is too long!')
+			.required('Name is required!'),
+		email: Yup.string()
+			.email('Please enter a valid email!')
+			.required('Email is required!'),
+		username: Yup.string()
+			.min(4, 'Username is too short!')
+			.max(50, 'Username is too long!')
+			.matches(/^[a-zA-Z0-9-_]+$/, 'Username must have _alpha_numerics')
+			.required('Username is required!'),
+		password: Yup.string()
+			.min(8, 'Password is too short!')
+			.max(20, 'Password is too long!')
+			.matches(
+				/(?=^.{8,20}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s)[0-9a-zA-Z!@#$%^&*()]*$/,
+				'Password must have atleast one lowercase letter, one uppercase letter, one digit' +
+					' and one special character.'
+			)
+			.required('Password is required!'),
+	})
+	return (
+		<div className="signup">
+			<h2 className="page-heading">
+				Register to start managing your expenses.
+			</h2>
+			<Formik
+				initialValues={{
+					username: '',
+					password: '',
+					name: '',
+					email: '',
+					gender: 'M',
+				}}
+				validationSchema={() => setAuthError(null) || SignupSchema}
+				onSubmit={(values, { setSubmitting }) => {
+					setTimeout(() => {
+						signupUser({
+							variables: {
+								username: values.username,
+								password: values.password,
+								name: values.name,
+								email: values.email,
+								gender: values.gender,
+							},
+						})
+						setSubmitting(false)
+					}, 400)
+				}}
+			>
+				{({
+					values,
+					errors,
+					touched,
+					handleChange,
+					handleBlur,
+					handleSubmit,
+					isSubmitting,
+				}) => (
+					<form onSubmit={handleSubmit}>
+						{authError &&
+							authError.map(err => (
+								<span
+									key={err}
+									id="username-error"
+									className="error-message"
+								>
+									{err}
+								</span>
+							))}
+						<div className="field">
+							<input
+								type="text"
+								placeholder="Enter your name"
+								id="name-input"
+								onChange={handleChange}
+								onBlur={handleBlur}
+								name="name"
+								value={values.name}
+							/>
+							<label htmlFor="name-input">
+								<i className="material-icons">account_circle</i>
+							</label>
+						</div>
+						{touched.name && errors.name && (
+							<span id="name-error" className="error-message">
+								{errors.name}
+							</span>
+						)}
+						<div className="field">
+							<input
+								onChange={handleChange}
+								onBlur={handleBlur}
+								value={values.email}
+								type="text"
+								name="email"
+								placeholder="Enter your email"
+								id="email-input"
+							/>
+							<label htmlFor="email-input">
+								<i className="material-icons">email</i>
+							</label>
+						</div>
+						{touched.email && errors.email && (
+							<span id="email-error" className="error-message">
+								{errors.email}
+							</span>
+						)}
+						<div className="field">
+							<input
+								onChange={handleChange}
+								onBlur={handleBlur}
+								value={values.username}
+								type="text"
+								name="username"
+								placeholder="Enter your username"
+								id="username-input"
+							/>
+							<label htmlFor="username-input">
+								<i className="material-icons">
+									alternate_email
+								</i>
+							</label>
+						</div>
+						{touched.username && errors.username && (
+							<span id="username-error" className="error-message">
+								{errors.username}
+							</span>
+						)}
+						<div className="field">
+							<input
+								onChange={handleChange}
+								onBlur={handleBlur}
+								value={values.password}
+								type="password"
+								name="password"
+								placeholder="Enter your password"
+								id="password-input"
+							/>
+							<label htmlFor="password-input">
+								<i
+									className="material-icons"
+									style={{
+										cursor: 'pointer',
+									}}
+									onClick={e => showPassword(e)}
+								>
+									visibility_off
+								</i>
+							</label>
+						</div>
+						{touched.password && errors.password && (
+							<span id="password-error" className="error-message">
+								{errors.password}
+							</span>
+						)}
+						<div className="field select-field">
+							<select
+								onChange={handleChange}
+								onBlur={handleBlur}
+								className="select-dropdown"
+								value={values.gender}
+								name="gender"
+							>
+								<option value="M">Male</option>
+								<option value="F">Female</option>
+							</select>
+						</div>
+						<button
+							type="submit"
+							className="btn btn__primary"
+							disabled={isSubmitting}
+						>
+							{mutationLoading ? 'Signing Up...' : 'Sign Up'}
+						</button>
+					</form>
+				)}
+			</Formik>
+		</div>
+	)
 }
+
+export default SignUp
